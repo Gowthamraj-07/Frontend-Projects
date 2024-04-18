2be64a50eb489a7643fe6e9f05bc439b1e794819
GowthamRaj T <150216248+Gowthamraj-07@users.noreply.github.com>
2024-04-18 21:26:04 +0530

    Add files via upload

---
+++ Xplore Project/script.js
@@ -0,0 +1,9 @@
+function redirect()
+{
+    let val=document.getElementById("value");
+    
+    if(val.value.toLowerCase()=="india")
+    {
+        location.href="https://www.w3schools.com/howto/howto_js_redirect_webpage.asp"
+    }
+}
